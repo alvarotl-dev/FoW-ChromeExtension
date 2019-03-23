c9de4acc51bf9953253aaa0398532f1a31424cdc
Álvaro Tomás Lozano <auberontermeraz@gmail.com>
2019-03-23 03:10:00 +0100

    1.0.3
    Commented code. Added features about searching, changed button names and color, added text box filter, repositioned buttons.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var isGachalogCode;
+/*****************************************************************************************************************************************************************/
+//**************************************************FUNCTIONS****************************************************************************************************/
+/***************************************************************************************************************************************************************/
 
-window.onload = function() {
-  document.getElementById("searchBox").focus();
-};
 
-function parseInformation() {
+window.onload = function(){		//Open the extension with the text box already
+	document.getElementById("searchBox").focus();
+}
+
+
+function parseInformation() {	//Parse the information on the checkbox to an alternate text of an HTML element
+
 	var specialCharacters = [["æ", "ae"], ["â", "a"], ["á", "a"], ["à", "a"], ["é", "e"], ["ö", "o"], ["û", "u"], ["ú", "u"]];
 	var cardName = document.getElementById("searchBox").value.toLowerCase();
 	
 	//Store card name in alt
-	document.getElementById("cardImage").alt = cardName;
+	document.getElementById("searchBox").alt = cardName;
 
 	//Replace special characters
 	for(var i = 0; i < cardName.length; i++) {
@@ -19,56 +24,91 @@ function parseInformation() {
 			}
 		}
 	}
-	
-	
 }
 
-function enterPressed(){
-	parseInformation();
-	return (parseInt(document.getElementById("cardImage").alt) != NaN && parseInt(document.getElementById("cardImage").alt).toString().length == 8)
+
+function displayButtons(isDisplayed){	//Display or hide the lower shop buttons
+	switch(isDisplayed)
+	{
+		case true: //Show the shop buttons
+			document.getElementById("CoolStuffButton").style.display = "inline-block";
+			document.getElementById("TCGPlayerButton").style.display = "inline-block";
+			document.getElementById("CardMarketButton").style.display = "inline-block";
+			break;	
+
+		case false: //Hide them if activated or do nothing
+			document.getElementById("CoolStuffButton").style.display = "none";
+			document.getElementById("TCGPlayerButton").style.display = "none";
+			document.getElementById("CardMarketButton").style.display = "none";	
+			break;	
+	}
 }
 
+
+function isGachalog(){	//First, parse to an integer the text of the box and check if is a Number and after if that number is 8 digits long, you get that's a gachalog code.  
+	return (parseInt(document.getElementById("searchBox").alt) != NaN && parseInt(document.getElementById("searchBox").alt).toString().length == 8)
+}
+
+
+function checkTextBox(){	//Parse the information on the text box and then check if it's empty.
+	parseInformation()
+	return document.getElementById("searchBox").alt != "";
+}
+
+
+/*****************************************************************************************************************************************************************/
+//**************************************************EVENT HANDLING***********************************************************************************************/
+/***************************************************************************************************************************************************************/
+
+//Redirect to your player dashboard on fowsystem.com
 document.getElementById("playerButton").onclick = function() {
 	chrome.tabs.create({ url: "https://www.fowsystem.com/es/player/dashboard"});
 };
 
+
+//Open the gachalog main page if texbox is empty or isn't a gachalog code
 document.getElementById("gachalogButton").onclick = function() {
-	isGachalogCode = true;
+
 	parseInformation()
 	
-	if(document.getElementById("cardImage").alt == "")
+	if(document.getElementById("searchBox").alt == "" || !isGachalog())
 		chrome.tabs.create({ url: "https://www.gachalog.com/"});
 	else
-		chrome.tabs.create({ url: "https://www.gachalog.com/list/" + document.getElementById("cardImage").alt});
+		chrome.tabs.create({ url: "https://www.gachalog.com/list/" + document.getElementById("searchBox").alt});
 };
 
+//Redirect to CoolStuff
 document.getElementById("CoolStuffButton").onclick = function() {
-	chrome.tabs.create({ url: "http://www.coolstuffinc.com/main_search.php?pa=searchOnName&page=1&resultsPerPage=25&q=" + document.getElementById("cardImage").alt });
+	chrome.tabs.create({ url: "http://www.coolstuffinc.com/main_search.php?pa=searchOnName&page=1&resultsPerPage=25&q=" + document.getElementById("searchBox").alt });
 };
-
+//Redirect to TCGPlayer
 document.getElementById("TCGPlayerButton").onclick = function() {
-	chrome.tabs.create({ url: "http://shop.tcgplayer.com/force-of-will/product/show?ProductName=" + document.getElementById("cardImage").alt });
+	chrome.tabs.create({ url: "http://shop.tcgplayer.com/force-of-will/product/show?ProductName=" + document.getElementById("searchBox").alt });
 };
-
+//Redirect to CardMarket
 document.getElementById("CardMarketButton").onclick = function() {
-	chrome.tabs.create({ url: "https://www.cardmarket.com/es/FoW/Products/Search?searchString=" + document.getElementById("cardImage").alt });
+	chrome.tabs.create({ url: "https://www.cardmarket.com/es/FoW/Products/Search?searchString=" + document.getElementById("searchBox").alt });
 };
 
 document.getElementById("searchButton").onclick = function() {
+	//First parse the information of the textbox.
 	parseInformation()
-	
-	document.getElementById("CoolStuffButton").style.display = "inline-block";
-	document.getElementById("TCGPlayerButton").style.display = "inline-block";
-	document.getElementById("CardMarketButton").style.display = "inline-block";
-	
+
+	//If it's not empty, show the buttons.
+	if(document.getElementById("searchBox").alt != "")
+	{
+		displayButtons(true);
+	}
 };
 
 document.getElementById("searchBox").onkeypress = function(key) {
-	if(key.keyCode == 13) {
-		if(isGachalogCode = enterPressed())
-			chrome.tabs.create({ url: "https://www.gachalog.com/list/" + document.getElementById("cardImage").alt });
+	if(key.keyCode == 13 && checkTextBox()) {
 
-		else
-			chrome.tabs.create({ url: "https://www.cardmarket.com/es/FoW/Products/Search?searchString=" + document.getElementById("cardImage").alt });
+		if(isGachalog())  //If its a gachalog code, search for it.
+			chrome.tabs.create({ url: "https://www.gachalog.com/list/" + document.getElementById("searchBox").alt });
+
+		else	//If not, it means is a text, so a card, then search for it on the CardMarket (by default the most used webpage)
+			chrome.tabs.create({ url: "https://www.cardmarket.com/es/FoW/Products/Search?searchString=" + document.getElementById("searchBox").alt });
+
 	}
 };
